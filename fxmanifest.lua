3a9a9110dd3eefba0d2d8f7d64faab6879acc4e1
itsHazeyo <72316312+itsHazeyo@users.noreply.github.com>
2023-02-10 18:59:36 -0500

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,7 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+games { 'gta5' }
+
+description 'ESX Badger Stocks'
 
 server_script '@mysql-async/lib/MySQL.lua'
 
